7a9c878212ec6e2e54d90539a9f85f0364c4dd86
kozleo <leokoz8@gmail.com>
2023-02-12 21:45:42 -0500

    first push

---
+++ scripts/request_jupyter.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+#SBATCH --job-name=jup_nb
+#SBATCH --mem=16GB
+#SBATCH --output=/om2/user/leokoz8/code/add_RL/results/slurm_out/%j.out
+
+#SBATCH --qos=normal
+#SBATCH --partition=normal
+
+##SBATCH --qos=fietelab
+##SBATCH --partition=fietelab
+
+#SBATCH --gres=gpu:1
+#SBATCH --time=0-12:00:00
+
+cd /om2/user/leokoz8/code/boa_rnn
+unset XDG_RUNTIME_DIR
+#conda activate boa_rnn
+jupyter notebook --ip=0.0.0.0 --port=9000 --no-browser --NotebookApp.token='' --NotebookApp.password=''
