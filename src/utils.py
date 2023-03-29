e0526c883af84e841c8d44e38df2049977708887
kozleo <leokoz8@gmail.com>
2023-03-28 21:36:49 -0400

    policy gradient example working on CocaineWorld

--- src/utils.py
+++ src/utils.py
@@ -4,7 +4,7 @@ from tqdm import tqdm
 import sys
 from gymnasium import Env, spaces
 from typing import List, Optional
-
+import numpy as np
 
 def save_frames_as_gif(
     frames,
@@ -61,3 +61,86 @@ class AddictionEnv(Env):
             self.render()
         return int(self.s), {"prob": 1}
 """
+
+
+def update_Q_function(Q,s,s_prime,a,r,gamma,alpha):
+
+    delta = r + gamma*np.max(Q[s_prime,:]) - Q[s,a]
+    Q[s,a] += alpha*delta
+    
+    return Q
+
+def update_Q_and_E(Q,E,s,s_prime,a,r,gamma,alpha,lam,d):
+    
+    # Update eligibility trace and Q-function
+    delta = reward + gamma * Q[next_state, next_action] - Q[state, action]
+    E[state, action] += 1
+    for s in range(env.observation_space.n):
+        for a in range(env.action_space.n):
+            Q[s, a] += alpha * delta * E[s, a]
+            E[s, a] = gamma * lambd * E[s, a]
+
+    return Q,E
+
+def epsilon_greedy_policy(s,Q,epsilon,num_actions):
+    if np.random.uniform(0,1) > epsilon:
+        a = np.argmax(Q[s,:])            
+    else:
+        a = np.random.choice(np.arange(num_actions))
+        
+    return a
+
+def step(s,a):
+    
+    if s == 0 and a == 0:
+        drug_press = True
+    else:
+        drug_press = False
+    
+    s_prime = 1           
+        
+    return s_prime, drug_press
+        
+
+
+def dopamine_release(t, release_type = 'quick_rise',k = 1,tau = 1,sigma = 1):
+    if release_type == 'quick_rise':
+        return np.maximum(0,k*tau*t*np.exp(-tau*t))
+    if release_type == 'gauss':
+        return np.maximum(0,k*np.exp(-(tau*t/sigma)**2))
+
+
+from scipy.special import softmax               
+
+def v_hat(w,S):
+    return np.dot(w,S)    
+    
+def update_actor_critic(S,a,z_w,z_theta,w,theta,I,R):
+
+    lam_theta = 0.9
+    lam_w = 0.9
+    
+    alpha_theta = 0.5
+    alpha_w = 0.5
+
+    gamma = 0.9
+
+    delta = R  + gamma*v_hat(w,S) - v_hat(w,S)
+
+    z_w = gamma*lam_w*z_w + S
+
+
+    one_hot_action = np.zeros(len(z_theta))
+    one_hot_action[a] = 1
+    z_theta = gamma*lam_theta*z_theta + I*(one_hot_action - softmax(theta))
+
+    w = w + alpha_w*delta*z_w
+    theta = theta + alpha_theta*delta*z_theta
+
+    I = gamma*I
+
+    return z_w,z_theta,w,theta,I
+
+
+
+    
